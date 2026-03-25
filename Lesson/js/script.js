fb2a6873488d7289d11df8ca761ec70a1e783121
Ilnytskyy Markiian <ilnytskyymarkiian@gmail.com>
2026-03-25 13:24:27 +0200

    publish homework

--- Lesson/js/script.js
+++ Lesson/js/script.js
@@ -67,20 +67,101 @@
 
 // addName(1, 3, 5, 67, 55);
 
-function ask(question, yes, no) {
-    if(confirm(question)) {
-        yes();
-    } else {
-        no();
-    }
-}
+// function ask(question, yes, no) {
+//     if(confirm(question)) {
+//         yes();
+//     } else {
+//         no();
+//     }
+// }
 
-function showOk() {
-    console.log("You say ok");
-}
+// function showOk() {
+//     console.log("You say ok");
+// }
 
-function showCancel() {
-    console.log("You say No");
-}
+// function showCancel() {
+//     console.log("You say No");
+// }
 
-ask("Yes or no?", showOk, showCancel);
+// ask("Yes or no?", showOk, showCancel);
+
+// const hello = function(name) {
+//     console.log(`Hello, ${name}`);
+    
+// }
+
+// const searchName = function(callback) {
+//     const name = "Ivan";
+//     callback(name);
+// }
+
+// searchName(hello);
+
+// function checkAge(age) {
+//     if(age > 18) {
+//         return console.log("Hello");
+//     }
+    
+//     return console.log("Goodbye");
+// }
+
+// checkAge(21);
+
+// function showMovie(age) {
+//     if(age < 21) {
+//         return;
+//     }
+
+//     return console.log("Go to movie!");
+// }
+
+// showMovie(21);
+
+// const test = (arg) => {
+//     console.log(arg);
+// };
+
+// test("hello");
+
+// const showMovie = (age) => {
+//     if(age < 21) {
+//         return;
+//     }
+
+//     return console.log("Go to movie!");
+// };
+
+// showMovie(12);
+
+// const a = () => {
+//     console.log("a");
+// };
+
+// const b = () => {
+//     console.log("b");
+// }
+
+// const c = () => {
+//     console.log("c");
+//     a();
+//     b();
+// };
+
+// c();
+
+// console.log(document);
+
+// const addNumber = (number) => {
+//     let sum = parseInt(number) + 10;
+//     return console.log(sum);
+// };
+
+// const numberRef = document.querySelector("input[name='numer']");
+
+// const buttonRef = document.querySelector("button");
+
+// buttonRef.addEventListener("click", () => {
+//     addNumber(numberRef.value);
+// });
+
+// console.log(buttonRef);
